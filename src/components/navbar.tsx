7b7b6871164b04b22d15a75cb0091b54aa08c562
Furkan Cengiz <muhammedcengiz1@gmail.com>
2024-01-02 18:52:58 +0300

    add: shadcn/ui and tailwind, theme provider and mode-toggle button

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ModeToggle } from './mode-toggle'
+export default function Navbar() {
+  return (
+    <nav>
+        <header>
+            <ModeToggle />
+        </header>
+    </nav>
+  )
+}
