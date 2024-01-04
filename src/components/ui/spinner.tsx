88a62a6d8c662c681fe73d0dacb9ebfb01159121
Furkan Cengiz <muhammedcengiz1@gmail.com>
2024-01-05 02:37:33 +0300

    ref: removed unnecessary prop in Spinner component

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/lib/utils'
 import React from 'react'
 type SpinnerProps = React.HTMLAttributes<HTMLDivElement>
-const Spinner = React.forwardRef<HTMLDivElement, SpinnerProps>(({ className, ...props }, ref) => {
+const Spinner = React.forwardRef<HTMLDivElement, SpinnerProps>(({ className }, ref) => {
     return (
         <div ref={ref} className={cn(
             'animate-spin rounded-full h-16 w-16 border-4 border-b-transparent border-s-foreground',
